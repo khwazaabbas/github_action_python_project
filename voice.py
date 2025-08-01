54342ce131d7b85facec75c70b5e123702a6efe0
khwazaabbas <khwazaabbas1995@gmail.com>
2025-08-01 12:23:23 +0530

    adding some text to speech programmed files

---
+++ voice.py
@@ -0,0 +1,63 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(''' 
+Every night in my dreams
+
+I see you, I feel you
+
+That is how I know you go on
+
+Far across the distance
+
+And spaces between us
+
+You have come to show you go on
+
+Near, Far,
+
+Wherever you are,
+
+I believe that the heart does go on
+
+Once more, you opened the door
+
+Rs 1 Trial
+And you're here in my heart and
+
+My heart will go on and on
+
+Love can touch us one time
+
+And last for a lifetime
+
+And never let go till we're gone
+
+Love was when I loved you,
+
+One true time I hold you,
+
+In my life we'll always go on
+
+Near, far,
+
+Wherever you are,
+
+I believe that the heart does go on
+
+Once more, you opened the door
+
+And you're here in my heart and
+
+My heart will go on and on
+
+You're here, there's nothing I fear
+
+And I know that my heart will go on
+
+We'll stay, forever this way
+
+You are safe in my heart and
+
+My heart will go on and on
+''')
+engine.runAndWait()
