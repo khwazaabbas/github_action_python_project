97fc2bde7af0eabe1d7ec206935834bee1f40c76
khwazaabbas <khwazaabbas1995@gmail.com>
2025-08-06 15:49:17 +0530

    add some shayri on voice programme

--- voice.py
+++ voice.py
@@ -1,63 +1,25 @@
 import pyttsx3
 engine = pyttsx3.init()
 engine.say(''' 
-Every night in my dreams
+john alia sahab khte hai
 
-I see you, I feel you
+k ab sirf aadaton ki warzish hai
+ruh shamil nhi hai shikayat main
 
-That is how I know you go on
+fir khte hai
 
-Far across the distance
+kya kaha mujhse milne aaye ho
+theron bula k aata hu
 
-And spaces between us
+fir khte hai
 
-You have come to show you go on
+uski hasratein naaz ka mujhse ye armaan the k umar guzaar dijiye
+umar guzar di gyi
 
-Near, Far,
+shayar farmaate hai
 
-Wherever you are,
+main to har dhoop main saayon ka raha hu juya
+mujhse likhwai sarabon ki kahani tumne
 
-I believe that the heart does go on
-
-Once more, you opened the door
-
-Rs 1 Trial
-And you're here in my heart and
-
-My heart will go on and on
-
-Love can touch us one time
-
-And last for a lifetime
-
-And never let go till we're gone
-
-Love was when I loved you,
-
-One true time I hold you,
-
-In my life we'll always go on
-
-Near, far,
-
-Wherever you are,
-
-I believe that the heart does go on
-
-Once more, you opened the door
-
-And you're here in my heart and
-
-My heart will go on and on
-
-You're here, there's nothing I fear
-
-And I know that my heart will go on
-
-We'll stay, forever this way
-
-You are safe in my heart and
-
-My heart will go on and on
 ''')
 engine.runAndWait()
